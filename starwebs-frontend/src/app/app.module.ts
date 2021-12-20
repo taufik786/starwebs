2e805128f8f2371f7b4a03d559c315929adb9109
Taufik Alam <taufikalam759@gmail.com>
2021-12-21 00:21:59 +0530

    nav added

--- starwebs-frontend/src/app/app.module.ts
+++ starwebs-frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
