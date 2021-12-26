4d22eba343242a00676ba6bbda5c6cf3e8eece37
Taufik Alam <taufikalam759@gmail.com>
2021-12-26 19:14:51 +0530

    inprogress

--- starwebs-frontend/src/app/app-routing.module.ts
+++ starwebs-frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'articles', component: ArticlesComponent},
+  {path: 'articles/:name', component: ArticlesComponent},
 ];
 
 @NgModule({
--- starwebs-frontend/src/app/app.module.ts
+++ starwebs-frontend/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    ServicesComponent,
+    AboutComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
